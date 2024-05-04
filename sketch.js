dc92b18fa03e02911ad4250a9074cbf197efa3df
KyleSaki714 <kyle13san@Gmail.com>
2024-05-04 16:56:29 -0700

    ORBIT WORKINGgit add .git add .

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 
-const ROTATE_CAMERA_SPEED = 2.5;
+const ROTATE_CAMERA_SPEED = 1;
 
 let _boxx = 0;
 let _boxy = 0;
@@ -7,39 +7,62 @@ let _curMouseRotate = 0; // current rotation along the y axis mapped to mouseX
 let _lastMouseRotate = 0;
 let _lastCameraPos;
 
+let myFont;
+function preload() {
+  myFont = loadFont("resources/Litebulb 8-bit.ttf");
+}
 
 function setup() {
   createCanvas(640, 480, WEBGL);
   frameRate(60); // 24 is stop-motion
   _lastCameraPos = createVector(0, 0, 800);
+  textFont(myFont);
+  textSize(36);
 }
 
-// theta (int): 0 - 360
-// cameraPos (p5.vector): position of camera
-// center (p5.vector): point the camera is pointing at
-// radius (int): camera distance from center
-// function calcCameraPosition(theta, cameraPos, center, radius) {
-//   let t = radians(theta);
-//   console.log(cameraPos);
-//   let x = (cameraPos.x - center.x) * cos(t) - (cameraPos.y - center.y) * sin(t) + center.x;
-//   let y = (cameraPos.x - center.x) * sin(t) + (cameraPos.y - center.y) * cos(t) + center.y;
-//   let z = center.z // + radius * math.sin(rad);
-//   // console.log(z);
-//   let res = createVector(x, y, z);
-//   // console.log(res);
-//   return res;
-// }
-function calcCameraPosition(theta, cameraPos, center, radius) {
+/**
+ * Rotates the camera around the Y axis.
+ * Credit to this video https://youtu.be/E9MnSQ9_hk0
+ * @param {Number} theta rotation 
+ * @param {p5.Vector} cameraPos position of camera
+ * @param {p5.Vector} center point the camera is pointing at
+ * @param {Number} radius camera distance from center
+ */
+function rotateCameraAroundPointY(theta, cameraPos, center, radius) {
   let t = radians(theta);
   // console.log(t);
   // console.log(cameraPos);
-  let x = (cameraPos.x * cos(t)) - ((cameraPos.z) * sin(t));
-  let z = (cameraPos.x * sin(t)) + ((cameraPos.z) * cos(t));
-  let y = 0; // + radius * math.sin(rad);
+  let x = ((cameraPos.x - center.x) * cos(t)) - ((cameraPos.z - center.z) * sin(t)) + center.x;
+  let z = ((cameraPos.x - center.x) * sin(t)) + ((cameraPos.z - center.z) * cos(t)) + center.y;
+  let y = cameraPos.y; // + radius * math.sin(rad);
   // console.log("x:" + x + " y: " + y + " z: " + z);
-  let res = createVector(x, y, z);
+  let newCameraPos = createVector(x, y, z);
   // console.log(res);
-  return res;
+  camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
+  _lastCameraPos = newCameraPos;
+}
+
+function rotateCameraAroundPointX(theta, cameraPos, center, radius) {
+  let t = radians(theta);
+  // console.log(t);
+  // console.log(cameraPos);
+  let y = ((cameraPos.y - center.y) * cos(t)) - ((cameraPos.z - center.z) * sin(t)) + center.x;
+  let z = ((cameraPos.y - center.y) * sin(t)) + ((cameraPos.z - center.z) * cos(t)) + center.y;
+  let x = cameraPos.x; // + radius * math.sin(rad);
+  // console.log("x:" + x + " y: " + y + " z: " + z);
+  let newCameraPos = createVector(x, y, z);
+  // console.log(res);
+  camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
+  _lastCameraPos = newCameraPos;
+}
+
+/**
+ * Will rotate the camera around the Y axis
+ * speed multiplier * 
+ * @param {Number} speed 
+ */
+function rotateCameraAroundOrigin(speed) {
+  rotateCameraAroundPointY(ROTATE_CAMERA_SPEED * speed, _lastCameraPos, createVector(0,0,0), 800);
 }
 
 function draw() {
@@ -49,22 +72,29 @@ function draw() {
   _curMouseRotate = (mouseX / width) * 360;
 //   // let x = _curMouseRotate * cos(frameCount * 0.01);
   
-  if (_lastMouseRotate !== _curMouseRotate) {
-    console.log(_curMouseRotate)
-    let newCameraPos = calcCameraPosition(-_curMouseRotate, _lastCameraPos, createVector(0,0,0), 800);
-    camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
-    _lastCameraPos = newCameraPos;
+  // if (_lastMouseRotate !== _curMouseRotate) {
+  //   console.log(_curMouseRotate)
+  //   let newCameraPos = rotateCameraAroundPoint(-_curMouseRotate, _lastCameraPos, createVector(0,0,0), 800);
+  //   camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
+  //   _lastCameraPos = newCameraPos;
     
+  // }
+
+  let mouseVelx = mouseX - pmouseX;
+  let mouseVely = mouseY - pmouseY;
+  if ((mouseVelx !== 0 || mouseVely !== 0) && mouseIsPressed) {
+    rotateCameraAroundPointY(ROTATE_CAMERA_SPEED * mouseVelx, _lastCameraPos, createVector(0,0,0), 800);
+    rotateCameraAroundPointX(ROTATE_CAMERA_SPEED * mouseVely, _lastCameraPos, createVector(0,0,0), 800);
+    // rotateCameraAroundOrigin(mouseVelx);
+    // console.log("mouse move xvel: " + mouseVelx);
   }
   
   if (keyIsDown(RIGHT_ARROW)) {
-        console.log("right");
-    let newCameraPos = calcCameraPosition(ROTATE_CAMERA_SPEED, _lastCameraPos, createVector(0,0,0), 800);
-    camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
-    _lastCameraPos = newCameraPos;
+    console.log("right");
+    rotateCameraAroundOrigin()
   } else if (keyIsDown(LEFT_ARROW)) {
     console.log("left");
-    let newCameraPos = calcCameraPosition(-ROTATE_CAMERA_SPEED, _lastCameraPos, createVector(0,0,0), 800);
+    let newCameraPos = rotateCameraAroundPointY(-ROTATE_CAMERA_SPEED, _lastCameraPos, createVector(0,0,0), 800);
     camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
     _lastCameraPos = newCameraPos;
   }
@@ -78,27 +108,15 @@ function draw() {
   pop(); 
   
   push();
-  translate(0,150,0);
   fill("green")
-  circle(0, 100, 15);
+  translate(0, 50, 0);
+  rotateX(radians(270));
+  rectMode(CENTER);
+  text("bottom", 0, 0);
+  // circle(0, 100, 15);
   pop(); 
   
   // rotateY(radians(_boxx));
   box(100, 90);
   // _boxx = (_boxx + 1) % 360 ;
-}
-
-
-function keyPressed() {
-  // if (keyCode === LEFT_ARROW) {
-  // console.log("left");
-  // } else if (keyCode === RIGHT_ARROW) {
-  //   console.log("right");
-  //   let newCameraPos = calcCameraPosition(1, _lastCameraPos, createVector(0,0,0), 800);
-  //   camera(newCameraPos.x, newCameraPos.y, newCameraPos.z);
-  //   _lastCameraPos = newCameraPos;
-  // }
-  
-
-
 }
